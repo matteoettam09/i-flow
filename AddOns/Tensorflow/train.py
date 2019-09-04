89d847f75f6881606554680fc0ae74c1cd36d93c
Joshua Isaacson <isaacson@fnal.gov>
2019-09-04 16:44:58 -0500

    Update train.py

--- AddOns/Tensorflow/train.py
+++ AddOns/Tensorflow/train.py
@@ -12,14 +12,14 @@ tfb = tfp.bijectors
 tfd = tfp.distributions
 
 FLAGS = flags.FLAGS
-flags.DEFINE_float('lr', 5e-3, 'Learning rate')
+flags.DEFINE_float('lr', 1e-3, 'Learning rate')
 flags.DEFINE_integer('epochs', 100, 'Number of epochs', short_name='e')
 flags.DEFINE_integer('nsamples', 1000, 'Number of samples per epoch', short_name='s')
 flags.DEFINE_integer('nlayers', 4, 'Number of bijector layers', short_name='l')
 flags.DEFINE_bool('eager', None, 'Whether to execute eagerly')
 flags.DEFINE_bool('acceptance', None, 'Whether to calculate acceptance after training', short_name='a')
-flags.DEFINE_integer('printout', 10, 'How often to print out the status of the loss', short_name='p')
-flags.DEFINE_bool('plot', None, 'Whether to plot the corner plots during training')
+flags.DEFINE_integer('printout', 10, 'How often to print out the status of the loss')
+flags.DEFINE_bool('plot', None, 'Whether to plot the corner plots during training', short_name='p')
 flags.DEFINE_bool('matrix', None, 'Whether to brute force plot the matrix elements', short_name='m')
 
 sherpa_module = tf.load_op_library('.libs/libSherpaTF.so')
@@ -27,10 +27,10 @@ call_sherpa = sherpa_module.call_sherpa
 
 def build_dense(in_features, out_features):
     invals = tf.keras.layers.Input(in_features, dtype=tf.float64)
-    h = tf.keras.layers.Dense(128, activation='relu', kernel_initializer='zeros')(invals)
-    h = tf.keras.layers.Dense(128, activation='relu', kernel_initializer='zeros')(h)
-#    h = tf.keras.layers.Dense(128, activation='relu', kernel_initializer='zeros')(h)
-#    h = tf.keras.layers.Dense(128, activation='relu', kernel_initializer='zeros')(h)
+    h = tf.keras.layers.Dense(128, activation='relu')(invals)
+    h = tf.keras.layers.Dense(128, activation='relu')(h)
+    h = tf.keras.layers.Dense(128, activation='relu')(h)
+    h = tf.keras.layers.Dense(128, activation='relu')(h)
     outputs = tf.keras.layers.Dense(out_features, kernel_initializer='zeros')(h)
     model = tf.keras.models.Model(invals, outputs)
     model.summary()
@@ -44,7 +44,7 @@ def mask_alternating(ndims, even=True):
     
 def mask_split(ndims, first=True):
     mask = np.zeros(ndims)
-    midpoint = ndims // 2 if ndims % 2 == 0 else ndims // 2 + 1
+    midpoint = ndims // 2
     if not first:
         mask[midpoint:] += 1
     else:
@@ -64,7 +64,7 @@ def build_network(masks, num_bins = 32, blob = None):
                                                               num_bins = num_bins,
                                                               blob = blob))
 
-    bijectors = tfb.Chain(list(bijectors))
+    bijectors = tfb.Chain(list(reversed(bijectors)))
    
     ndims = len(masks[0])
     low = np.zeros(ndims,dtype=np.float64)
@@ -99,7 +99,6 @@ def parse_input():
             return nparts
     except FileNotFoundError:
         raise FileNotFoundError('A Run.dat file is required.')
-    
 
 def main(argv):
     del argv
@@ -117,23 +116,34 @@ def main(argv):
 
     # Plot matrix element
     if FLAGS.matrix:
-        x = np.random.random((1000000,nrans))
-        p = call_sherpa(x)
-        figure = corner.corner(x, weights=p, labels = [r'$x_{{{}}}$'.format(x) for x in range(nrans)],
+        pts = []
+        weights = []
+        for _ in range(10):
+            x = np.random.random((100000,nrans))
+            pts.append(x)
+            weights.append(call_sherpa(x))
+
+        pts = np.concatenate(pts)
+        weights = np.concatenate(weights)
+
+        figure = corner.corner(pts, weights=weights, labels = [r'$x_{{{}}}$'.format(i) for i in range(nrans)],
                                show_titles=True, title_kwargs={'fontsize': 12},
                                range=nrans*[[0,1]], **hist2d_kwargs)
         plt.savefig('matrix.png')
         plt.close()
 
-    raise
-
     # Create the masks for the bijectors
+    channel_mask = np.zeros(nchannels)
+    dims_mask = np.zeros(ndims)
     masks = []
-    masks.append(mask_alternating(ndims))
-    masks.append(mask_alternating(ndims, False))
-    masks.append(mask_split(ndims))
-    masks.append(mask_flip(masks[-1]))
+    masks.append(np.concatenate([dims_mask,mask_alternating(nchannels)]))
+    masks.append(np.concatenate([dims_mask,mask_alternating(nchannels, False)]))
+    masks.append(np.concatenate([mask_alternating(ndims),channel_mask]))
+    masks.append(np.concatenate([mask_alternating(ndims, False),channel_mask]))
+    masks.append(np.concatenate([mask_split(ndims),channel_mask]))
+    masks.append(np.concatenate([mask_split(ndims, False),channel_mask]))
     masks = np.array(masks)
+    print(masks)
 
     # Build the integrator
     dist = build_network(masks)
@@ -144,7 +154,7 @@ def main(argv):
         for epoch in range(FLAGS.epochs):
             if epoch % printout == 0 and FLAGS.plot:
                 samples = integrate.sample(10000)
-                figure = corner.corner(samples, labels = ['\$x_\{{}\}\$'.format(x) for x in range(nrans)],
+                figure = corner.corner(samples, labels = [r'$x_{{{}}}$'.format(x) for x in range(nrans)],
                                        show_titles=True, title_kwargs={'fontsize': 12},
                                        range=nrans*[[0,1]], **hist2d_kwargs)
 
@@ -156,9 +166,12 @@ def main(argv):
                     figure.suptitle('loss = {:8e}'.format(loss.numpy()), fontsize=16, x=0.75)
                     plt.savefig('fig_{:04d}.png'.format(epoch))
                     plt.close()
+                    del figure
+                    del samples
     except KeyboardInterrupt:
         if FLAGS.plot:
             plt.close()
+    print('Epoch: {:4d} Loss = {:8e} Integral = {:8e} +/- {:8e}'.format(epoch, loss, integral, error))
 
     if FLAGS.acceptance:
         weights = []
@@ -170,6 +183,11 @@ def main(argv):
 
         print('Acceptance = {}'.format(average/max_wgt))
 
+        print(len(weights) - np.count_nonzero(weights), np.count_nonzero(weights))
+
+        weights = weights[np.nonzero(weights)]
+        print(len(weights) - np.count_nonzero(weights), np.count_nonzero(weights))
+
         print(np.min(weights), np.max(weights))
 
         plt.hist(weights, bins=np.logspace(np.log10(np.min(weights))-1e-2, 
@@ -177,7 +195,6 @@ def main(argv):
         plt.yscale('log')
         plt.xscale('log')
         plt.savefig('efficiency.png')
-
 
 if __name__ == '__main__':
     app.run(main)
